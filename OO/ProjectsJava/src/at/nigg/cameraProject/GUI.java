9e4d771eb17e2effe5a237993a904ebfd73fc3db
JonasN05 <jonas.nigg@gmx.at>
2024-10-24 12:07:26 +0200

    gui - run method added and take picture added

--- OO/ProjectsJava/src/at/nigg/cameraProject/GUI.java
+++ OO/ProjectsJava/src/at/nigg/cameraProject/GUI.java
@@ -1,5 +1,42 @@
 package at.nigg.cameraProject;
 
-public class GUI {
+import java.util.Scanner;
 
+public class GUI {
+    private Camera camera;
+    Scanner scanner = new Scanner(System.in);
+
+    public GUI(Camera camera) {
+        this.camera = camera;
+    }
+
+    public void runCamera() {
+        boolean guiRunning = true;
+        while (guiRunning) {
+            System.out.println("What would you like to do?");
+            System.out.println("Press 1 - Take Picture");
+            System.out.println("Press 2 - Add SD-Card");
+            System.out.println("Press 3 - Remove SD-Card");
+            System.out.println("Press 9 - Shut Down");
+            int s = scanner.nextInt();
+
+            switch (s) {
+                case 1:
+                    System.out.println("Enter the name of the picture:");
+                    String nameOfPicture = scanner.next();
+                    camera.takePicture(nameOfPicture);
+                    break;
+
+                case 9:
+                    guiRunning = false;
+                    break;
+
+                default:
+                    break;
+
+            }
+            System.out.println("-----------------------");
+
+        }
+    }
 }
--- OO/ProjectsJava/src/at/nigg/cameraProject/Main.java
+++ OO/ProjectsJava/src/at/nigg/cameraProject/Main.java
@@ -7,17 +7,8 @@ public class Main {
         Lens lens1 = new Lens(100, manufacturer1);
         Camera camera1 = new Camera(200, 200, "red", Resolution.HIGH, sdCard1, lens1, manufacturer1);
 
-        camera1.takePicture("pic1");
-        camera1.takePicture("pic2");
-
-        camera1.getFreeMemoryOfSD();
-
-        camera1.setResolution(Resolution.LOW);
-        camera1.takePicture("pic3");
-
-        camera1.takePicture("pic4");
-
-        camera1.getFreeMemoryOfSD();
+        GUI gui = new GUI(camera1);
+        gui.runCamera();
 
     }
 }
