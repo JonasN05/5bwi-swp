e19b759fd568f5a9ec02285fa6f680103dd3a21e
JonasN05 <jonas.nigg@gmx.at>
2024-11-07 10:52:12 +0100

    vscode in git-ignore added + gui finished

--- OO/ProjectsJava/src/at/nigg/cameraProject/test/CameraTest.java
+++ OO/ProjectsJava/src/at/nigg/cameraProject/test/CameraTest.java
@@ -1,21 +1,70 @@
 package at.nigg.cameraProject.test;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.support.discovery.SelectorResolver.Resolution;
 
 import at.nigg.cameraProject.Camera;
-import at.nigg.cameraProject.SDCard;
+import at.nigg.cameraProject.Resolution;
 
 public class CameraTest {
     @Test
-    void testTakePicture() {
-        SDCard sdCard1 = new SDCard(10);
-        // Camera camera1 = new Camera(200, 200, "red", Resolution.HIGH, sdCard1);
+    void testGetColorOfPhone() {
+        Camera camera = new Camera(200, 200, "red", Resolution.HIGH, null, null, null);
+        Assertions.
+    }
 
-        // camera1.takePicture("pic1");
-        // camera1.takePicture("pic2");
-        // Assertions.assertEquals(, camera1);
+    @Test
+    void testGetFreeMemoryOfSD() {
+
+    }
+
+    @Test
+    void testGetLens() {
+
+    }
+
+    @Test
+    void testGetManufacturer() {
+
+    }
+
+    @Test
+    void testGetPixel() {
+
+    }
+
+    @Test
+    void testGetResolution() {
+
+    }
+
+    @Test
+    void testGetSdCard() {
+
+    }
+
+    @Test
+    void testGetWeightOfPhone() {
+
+    }
+
+    @Test
+    void testRemoveSdCard() {
+
+    }
+
+    @Test
+    void testSetResolution() {
+
+    }
+
+    @Test
+    void testSetSdCard() {
+
+    }
+
+    @Test
+    void testTakePicture() {
+
     }
 }
