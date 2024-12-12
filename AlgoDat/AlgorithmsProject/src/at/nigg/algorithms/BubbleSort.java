cb021c06a6880da7e31f017d12c2199255d8de5e
JonasN05 <jonas.nigg@gmx.at>
2024-12-12 10:48:13 +0100

    BubbleSort and SelectionSort added

--- AlgoDat/AlgorithmsProject/src/at/nigg/algorithms/BubbleSort.java
+++ AlgoDat/AlgorithmsProject/src/at/nigg/algorithms/BubbleSort.java
@@ -2,6 +2,9 @@ package at.nigg.algorithms;
 
 public class BubbleSort implements Sorter {
 
+    // https://www.geeksforgeeks.org/bubble-sort-algorithm/
+    // https://www.w3schools.com/dsa/dsa_algo_bubblesort.php
+
     private String name;
 
     public BubbleSort(String name) {
@@ -10,7 +13,25 @@ public class BubbleSort implements Sorter {
 
     @Override
     public int[] sort(int[] array) {
-        return new int[] { 1, 2, 3 };
+        boolean swapped;
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Tauschen von zwei Elementen
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // Abbruch des Arrays sollte sich nichts verändern -> effizienter
+            if (swapped == false)
+                break;
+        }
+        return array;
     }
 
     @Override
--- AlgoDat/AlgorithmsProject/src/at/nigg/algorithms/SelectionSort.java
+++ AlgoDat/AlgorithmsProject/src/at/nigg/algorithms/SelectionSort.java
@@ -10,7 +10,20 @@ public class SelectionSort implements Sorter {
 
     @Override
     public int[] sort(int[] array) {
-        return new int[] { 3, 2, 1 };
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int indexMinValue = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[indexMinValue]) {
+                    indexMinValue = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[indexMinValue];
+            array[indexMinValue] = temp;
+        }
+
+        return array;
     }
 
     @Override
